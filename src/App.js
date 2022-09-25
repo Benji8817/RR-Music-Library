3a60056ed5f5b129f26e5438759f14993bb5a20b
Benji8817 <103083571+Benji8817@users.noreply.github.com>
2022-09-25 16:15:40 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import './App.css';
-import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
+import "./App.css";
+import { useEffect, useState } from "react";
+import Gallery from "./components/Gallery";
+import SearchBar from "./components/SearchBar";
+import { createResource as fetchData } from "./helper";
 
 function App() {
-  let [searchTerm, setSearchTerm] = useState('')
-  let [data, setData] = useState([])
-  let [message, setMessage] = useState('Search for Music!')
+  let [searchTerm, setSearchTerm] = useState("");
+  let [data, setData] = useState(null);
+  let [message, setMessage] = useState("Search for Music!");
 
   useEffect(() => {
     if (searchTerm) {
-      document.title=`${searchTerm} Music`
-      const fetchData = async () => {
-        const response = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`)
-        const resData = await response.json()
-        if(resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Not Found')
-        }
-      }
-      fetchData()
-  }
-  }, [searchTerm])
+      document.title = `${searchTerm} Music`;
+      setData(fetchData(searchTerm));
+    }
+  }, [searchTerm]);
 
   const handleSearch = (e, term) => {
-    e.preventDefault()
-    setSearchTerm(term)
-  }
+    e.preventDefault();
+    setSearchTerm(term);
+  };
 
   return (
     <div className="App">
